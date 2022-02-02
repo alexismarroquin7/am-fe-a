8534bf2b255a8db92eddfe8d8cc828ec0cab35d2
alexismarroquin7 <alexismarroquin754@gmail.com>
2022-02-02 04:18:46 -0800

    install and setup styled-components

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
+// style
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyles, theme } from "../styles";
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <ThemeProvider theme={theme}>
+    <GlobalStyles />
+    <Component {...pageProps} />
+  </ThemeProvider>
+
+  )
 }
 
 export default MyApp
